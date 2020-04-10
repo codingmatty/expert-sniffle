d43e3471fa62473802b5a81e85ed4b5a7993dd3b
Matty J <matty@joust.com>
2020-04-10 17:28:26 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./app/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 3030,
+    publicPath: "http://localhost:3030/dist/",
+    hotOnly: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
